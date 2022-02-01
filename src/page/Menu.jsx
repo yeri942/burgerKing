42155374acce429f7db0bb9e9b98e025f3331958
김예리 <dpfl0445@gmail.com>
2022-02-01 23:12:40 +0900

    create menu페이지 영역 잡기

--- src/page/Menu.jsx
+++ src/page/Menu.jsx
@@ -1,8 +1,30 @@
 import styled, { keyframes } from "styled-components";
 import { useState, useEffect } from "react";
+const MenuPageWrapper = styled.div`
+    height: 100vh;
+`;
+const HeaderWrapper = styled.header`
+    height: 12%;
+    background-color: yellow;
+`;
+const ContentWrapper = styled.div`
+    height: 80%;
+    background-color: blue;
+`;
+
+const FooterWrapper = styled.footer`
+    height: 8%;
+    background-color: yellow;
+`;
 
 const Menu = () => {
-    return <div></div>;
+    return (
+        <MenuPageWrapper>
+            <HeaderWrapper>Nav</HeaderWrapper>
+            <ContentWrapper>Content</ContentWrapper>
+            <FooterWrapper>footer</FooterWrapper>
+        </MenuPageWrapper>
+    );
 };
 
 export default Menu;
