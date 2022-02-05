7371901a94fb0cfcdfbfdaaa8ea1f78b77de2409
김예리 <dpfl0445@gmail.com>
2022-02-05 23:57:36 +0900

    create beef 버거 정보 파일

--- src/page/Menu.jsx
+++ src/page/Menu.jsx
@@ -16,10 +16,15 @@ const Nav = styled.div`
     justify-content: space-between;
     align-items: center;
 `;
-const NavZone = styled.span`
+const NavZone = styled.ul`
     margin-right: 30px;
     & > :not(:first-child) {
         margin-left: 30px;
+    }
+    & > li {
+        list-style: none;
+        float: left;
+        cursor: pointer;
     }
 `;
 const PrevButton = styled.button`
@@ -55,9 +60,9 @@ const Menu = () => {
                 <Nav>
                     <Kind>BEEF BURGER</Kind>
                     <NavZone>
-                        <span>Premium</span>
-                        <span>Whopper</span>
-                        <span>Junior&Burger</span>
+                        <li>Premium</li>
+                        <li>Whopper</li>
+                        <li>Junior&Burger</li>
                     </NavZone>
                 </Nav>
             </HeaderWrapper>
